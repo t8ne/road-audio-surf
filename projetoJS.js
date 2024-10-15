256d542d73b85a972914cae19b126de1fab36739
António Rebelo <imtoninho@gmail.com>
2024-10-15 16:39:37 +0100

    Final

--- projetoJS.js
+++ projetoJS.js
@@ -1,59 +1,92 @@
+//--Faixas, Posição, Obstáculos e Score
 let lanes = [150, 300, 450];
 let playerPos;
 let obstacles = [];
-let song;
-let backgroundSound;
-let winSound;
-let loseSound;
-let fft;
 let laneIndex = 1;
 let score = 0;
+
+//--Estados do Jogo
 let gamePaused = false;
 let gameOver = false;
+
+//--Diferentes Ecrâs
 let startScreen = true;
 let levelSelectScreen = false;
 let endScreen = false;
 let winScreen = false;
+let showPopup = false;
+
+//--Microfone
 let mic;
 let minMicLevel = 0.01;
-let gameTime = 0;
-let showPopup = false;
-let musicPlaying = false;
-let backgroundImg;
+let fft;
+
+//--Mecânicas do Jogo
 let obstacleTimer = 0;
 let obstacleInterval = 1000; // 1 segundo
 let gameTimer = 204000; //204000 - 2000;
+let gameTime = 0;
 let lastKeyPressTime = 0;
-let keyPressDelay = 150; // 130ms de atraso entre teclas
+let keyPressDelay = 150; // 150ms de atraso entre teclas
 let accumulatedScore = 0;
 let selectedLevel = 0;
+
+//--Áudio
+let song;
+let backgroundSound;
+let winSound;
+let loseSound;
+let musicPlaying = false;
+
+//--Imagens
 let levelImages = [];
 let topScores = [];
-let playerImg;
-let obstacleImg;
+let playerImages = [];
+let currentPlayerImage;
+let obstacleImages = [];
+let backgroundImg;
 
+//--Preload dos Sons e Imagens
 function preload() {
-  song = loadSound('media/musica.mp3');
-  backgroundSound = loadSound('media/background.mp3');
-  winSound = loadSound('media/win.mp3');
-  loseSound = loadSound('media/lose.mp3');
-  backgroundImg = loadImage('media/background.png');
-  playerImg = loadImage('media/player.png');
-  obstacleImg = loadImage('media/obstacle.png');
+
+  //--Carregar sons
+  song = loadSound('media/sounds/musica.mp3');
+  backgroundSound = loadSound('media/sounds/background.mp3');
+  winSound = loadSound('media/sounds/win.mp3');
+  loseSound = loadSound('media/sounds/lose.mp3');
+
+  //--Carregar Imagens
+  backgroundImg = loadImage('media/other/background.png');
+
   for (let i = 0; i < 6; i++) {
-    levelImages[i] = loadImage(`media/level${i+1}.jpg`);
+    playerImages[i] = loadImage(`media/players/player${i+1}.png`);
+  }
+
+  for (let i = 0; i < 3; i++) {
+    obstacleImages[i] = loadImage(`media/obstacles/obstacle${i+1}.png`);
+  }
+  for (let i = 0; i < 6; i++) {
+    levelImages[i] = loadImage(`media/levels/level${i+1}.jpg`);
   }
 }
 
+//--Setup do Cenário
 function setup() {
+
   createCanvas(600, 600);
+
+  //--Setup de Variáveis do player
   playerPos = createVector(lanes[laneIndex], height - 100);
+  currentPlayerImage = playerImages[0];
+
+  //--Setup de Microfone e Som
   fft = new p5.FFT();
   mic = new p5.AudioIn();
   mic.start();
   backgroundSound.loop();
 }
 
+//--Função Principal de todos os ecrâs
 function draw() {
   if (startScreen) {
     startGameScreen();
@@ -73,28 +106,37 @@ function draw() {
   }
 }
 
+//--Primeiro Ecrâ do Jogo
 function startGameScreen() {
+
+  //--Estilo do Ecrâ
   background(0);
   tint(255, 39);
   image(backgroundImg, 0, 0, width, height);
   noTint();
 
+  //--Estilo do Texto
   fill(255);
   textAlign(CENTER);
-  
   textSize(40);
   textStyle(BOLD);
+
+  //--1ª Parte de Texto
   text("BEM-VINDO AO ", width / 2 + 6, height / 4);
-  text("IPVC Audio Surf!", width / 2, height / 3);
+  text("IPVC Audio Road Surf!", width / 2, height / 3);
 
   textSize(20);
-  text("REGRAS: ", width / 2, height / 2);
+
+  //--2ª Parte de Texto
+  text("CONTROLOS: ", width / 2, height / 2);
   text("Use as setas esquerda/direita para mudar de faixa.", width / 2, height / 2 + 60);
   text("Canta com a música para ativar obstáculos", width / 2, height / 2 + 100);
   text("e ganhar Pontos!", width / 2, height / 2 + 125);
   text("Prima ENTER para começar", width / 2, height / 2 + 215);
+
   textStyle(NORMAL);
   
+  //-Avançar de Ecrâ
   if (keyIsPressed && key === 'Enter' && millis() - lastKeyPressTime > keyPressDelay) {
     startScreen = false;
     levelSelectScreen = true;
@@ -102,35 +144,51 @@ function startGameScreen() {
   }
 }
 
+//--Ecrâ de showcase dos níveis
 function showLevelSelectScreen() {
+
+  //--Estilo do Ecrâ
   background(0);
   tint(255, 128);
   image(backgroundImg, 0, 0, width, height);
   noTint();
 
+  //--Estilo do Texto
   fill(255);
   textAlign(CENTER);
   textSize(30);
+  textStyle(BOLD);
+
+  //--Texto do Ecrâ
   text("Escolha o seu nível", width / 2, 50);
   textSize(15);
   text("ENTER para confirmar", width / 2, height -50);
+  textStyle(NORMAL);
 
+  //--Organização das Imagens para os Níveis
   for (let i = 0; i < 6; i++) {
     let row = floor(i / 3);
     let col = i % 3;
-    image(levelImages[i], 100 + col * 150, 150 + row * 150, 100, 100);
+    image(levelImages[i], 35 + col * 191, 150 + row * 170, 150, 110);
     if (i === selectedLevel) {
+
+      //--Estilo do retângulo de Escolha
       noFill();
-      stroke(0);
+      stroke(255);
       strokeWeight(1.5);
-      rect(95 + col * 150, 145 + row * 150, 110, 110);
+      rect(30 + col * 191, 145 + row * 170, 160, 120);
       
-      // Desenhar seta
+      //--Desenhar seta de Escolha do Nível
       fill(255);
-      triangle(150 + col * 150, 270 + row * 150, 130 + col * 150, 290 + row * 150, 170 + col * 150, 290 + row * 150);
+      let arrowX = 110 + col * 191;
+      let arrowY = 273 + row * 170;
+      triangle(arrowX, arrowY, arrowX - 20, arrowY + 20, arrowX + 20, arrowY + 20);
+      stroke(0);
+
     }
   }
 
+  //--Lógica para escolha do Nível
   if (keyIsPressed && millis() - lastKeyPressTime > keyPressDelay) {
     if (keyCode === LEFT_ARROW && selectedLevel % 3 > 0) {
       selectedLevel--;
@@ -146,34 +204,47 @@ function showLevelSelectScreen() {
       backgroundSound.stop();
       song.play();
       musicPlaying = true;
+      
+      currentPlayerImage = playerImages[selectedLevel];
     }
     lastKeyPressTime = millis();
   }
 }
 
+//--Ecrâ de Game Over
 function endGameScreen() {
+
+  //--Estilo do Ecrâ
   background(0);
   tint(255, 39);
   image(backgroundImg, 0, 0, width, height);
   noTint();
 
+  //--Estilo do Texto
   fill(255, 0, 0);
   textAlign(CENTER);
-  
   textSize(40);
-  text("Game Over!", width / 2, height / 3);
+  textStyle(BOLD);
+  strokeWeight(0.5)
+  stroke(0);
+
+  text("Game Over!", width / 2, height / 4);
   
   textSize(20);
-  text("Pontuação Final: " + score, width / 2, height / 2);
+
+  text("Pontuação Final: " + score, width / 2, height / 3 - 10);
   
   // Mostrar pontuações mais altas
-  text("Top Pontuações:", width / 2, height / 2 + 40);
+  text("Top Pontuações:", width / 2, height / 2 - 15);
   for (let i = 0; i < Math.min(topScores.length, 3); i++) {
-    text((i + 1) + ". " + topScores[i], width / 2, height / 2 + 70 + i * 30);
+    text((i + 1) + ". " + topScores[i], width / 2, height / 2 + 25 + i * 30);
   }
   
-  text("Prima ENTER para voltar ao início", width / 2, height - 50);
+  text("Prima ENTER para voltar ao início", width / 2, height - 80);
 
+  textStyle(NORMAL);
+
+  //--Voltar para o ecrâ principal
   if (keyIsPressed && key === 'Enter' && millis() - lastKeyPressTime > keyPressDelay) {
     startScreen = true;
     endScreen = false;
@@ -182,49 +253,62 @@ function endGameScreen() {
   }
 }
 
+//--Ecrâ de Vitória do jogador
 function winGameScreen() {
+
+  //--Estilo do Ecrâ
   background(0);
   tint(255, 39);
   image(backgroundImg, 0, 0, width, height);
   noTint();
 
+  //--Estilo do Texto
   fill(0, 255, 0);
   textAlign(CENTER);
-  
   textSize(40);
-  text("Ganhou!", width / 2, height / 3);
+  textStyle(BOLD);
+  strokeWeight(0.5)
+  stroke(0);
+
+  text("You Win!", width / 2, height / 4);
   
   textSize(20);
-  text("Pontuação Final: " + score, width / 2, height / 2);
+
+  text("Pontuação Final: " + score, width / 2, height / 3 - 10);
   
   // Mostrar pontuações mais altas
-  text("Top Pontuações:", width / 2, height / 2 + 40);
+  text("Top Pontuações:", width / 2, height / 2 - 15);
   for (let i = 0; i < Math.min(topScores.length, 3); i++) {
-    text((i + 1) + ". " + topScores[i], width / 2, height / 2 + 70 + i * 30);
+    text((i + 1) + ". " + topScores[i], width / 2, height / 2 + 25 + i * 30);
   }
   
-  text("Prima ENTER para voltar ao início", width / 2, height - 50);
+  text("Prima ENTER para voltar ao início", width / 2, height - 80);
 
+  textStyle(NORMAL);
+
+  //--Voltar para o ecrâ principal
   if (keyIsPressed && key === 'Enter' && millis() - lastKeyPressTime > keyPressDelay) {
     startScreen = true;
-    winScreen = false;
+    endScreen = false;
     backgroundSound.loop();
     lastKeyPressTime = millis();
   }
 }
 
+//--Ecrâ do Jogo em si
 function playGame() {
-  // Desenhar fundo
+
+  //--Desenhar fundo
   image(backgroundImg, 0, 0, width, height);
   image(backgroundImg, 0, -height, width, height);
-  
-  // Mais do fundo
   backgroundImg.y = (backgroundImg.y + 5) % height;
   
+  //--Microfone
   let micLevel = mic.getLevel();
   
   gameTime += deltaTime;
   
+  //--Condições para o Jogador ganhar (Tempo acabar)
   if (gameTime >= gameTimer) {
     winScreen = true;
     song.stop();
@@ -233,6 +317,7 @@ function playGame() {
     return;
   }
   
+  //--Mecânica de falar para criar obstáculos e aumentar score
   if (micLevel > minMicLevel) {
     obstacleTimer += deltaTime;
     accumulatedScore += deltaTime / 1000;
@@ -242,25 +327,30 @@ function playGame() {
       for (let i = 0; i < 3; i++) {
         let lane = floor(random(3));
         let obstacle = createVector(lanes[lane], -50 - i * 100);
+        obstacle.imageIndex = floor(random(obstacleImages.length));
         obstacles.push(obstacle);
       }
       obstacleTimer = 0;
     }
   }
   
-  // Desenhar jogador
-  fill(0, 255, 0);
+  //--Desenhar o Player
+  fill(0, 255, 0, 0);
+  noStroke();
   rect(playerPos.x - 25, playerPos.y - 25, 50, 50);
-  image(playerImg, playerPos.x - 20, playerPos.y - 20, 40, 40);
+  image(currentPlayerImage, playerPos.x - 25, playerPos.y - 25, 50, 50);
   
+  //--Desenhar os obstáculos
   for (let i = obstacles.length - 1; i >= 0; i--) {
-    // Desenhar obstáculo
-    fill(255, 0, 0, 0);
-    noStroke();
-    rect(obstacles[i].x - 25, obstacles[i].y - 25, 50, 70);
-    image(obstacleImg, obstacles[i].x - 30, obstacles[i].y - 40, 60, 80);
-    obstacles[i].y += 5;
 
+    //--Estilo de um obstáculo
+    fill(255, 0, 0, 0);
+    rect(obstacles[i].x - 25, obstacles[i].y - 25, 50, 70);
+    image(obstacleImages[obstacles[i].imageIndex], obstacles[i].x - 30, obstacles[i].y - 40, 60, 80);
+    obstacles[i].y += 5;
+    noStroke();
+
+    //--Check para colisões entre o player e um obstáculo
     if (checkCollision(playerPos, obstacles[i])) {
       obstacles.splice(i, 1);
       endScreen = true;
@@ -270,34 +360,50 @@ function playGame() {
       return;
     }
     
+    //--Remoção de um obstáculo do ecrâ
     if (obstacles[i].y > height) {
       obstacles.splice(i, 1);
     }
   }
+
+  //--Estilo de texto para score e tempo
   stroke(0);
   fill(255);
   textSize(20);
   textAlign(LEFT);
+  textStyle(BOLD);
+
   text("Pontuação: " + score, 35, 30);
   textAlign(RIGHT);
   text("Tempo: " + floor((gameTimer - gameTime) / 1000) + "s", width - 30, 30);
   
-  // Informação do autor
+  //--Estilo para o autor
   textAlign(RIGHT);
   textSize(12);
   fill(200);
-  text("ESC para Menu de Pausa", width/4 + 21, height - 10);
+
+  //--Informação do autor
+  text("ESC para Menu de Pausa", width/4 + 24, height - 10);
   text("António Rebelo - Nº28837 - ECGM", width - 30, height - 10);
-  
+
+  textStyle(NORMAL);
+
+  //--Função para a mudança de faixas
   handlePlayerMovement();
 }
 
+//--Mudança de faixas
 function handlePlayerMovement() {
+
   if (keyIsPressed && millis() - lastKeyPressTime > keyPressDelay) {
+
     if (keyCode === LEFT_ARROW && laneIndex > 0) {
+
       laneIndex--;
       lastKeyPressTime = millis();
+
     } else if (keyCode === RIGHT_ARROW && laneIndex < 2) {
+
       laneIndex++;
       lastKeyPressTime = millis();
     }
@@ -307,6 +413,7 @@ function handlePlayerMovement() {
   playerPos.x = lerp(playerPos.x, targetX, 0.2);
 }
 
+//--Verificação de Colisões entre o Player e um Obstáculo através da posição de ambos
 function checkCollision(player, obstacle) {
   return (
     player.x - 25 < obstacle.x + 25 &&
@@ -316,19 +423,25 @@ function checkCollision(player, obstacle) {
   );
 }
 
+//--Popup de Menu de Pausa
 function pausePopup() {
+
+  //--Estilo de Texto e Retângulo
   fill(50, 50, 50, 200);
   rect(width / 4, height / 3, width / 2, height / 3);
-  
   fill(255);
-
   textAlign(CENTER);
   textSize(20);
+  textStyle(BOLD);
+
+  //--Texto do Menu
   text("Jogo Pausado", width / 2, height / 2 - 50);
-  text("Pressione:", width / 2, height / 2 - 5 );
-  text("- P para continuar", width / 2 - 5, height / 2 + 30);
-  text("- S para sair", width / 2 - 5, height / 2 + 55);
+  text("Pressione:", width / 2, height / 2 - 5);
+  text("- P para continuar", width / 2 - 5, height / 2 + 35);
+  text("- S para sair", width / 2 - 5, height / 2 + 60);
+  textStyle(NORMAL);
   
+  //--Verificação da saída ou continuação do jogo
   if (keyIsPressed && millis() - lastKeyPressTime > keyPressDelay) {
     if (key === 'p') {
       gamePaused = false;
@@ -344,13 +457,15 @@ function pausePopup() {
       lastKeyPressTime = millis();
     }
   }
+  
 }
 
+//--Função para reiniciar o jogo
 function resetGame() {
   song.stop();
   score = 0;
   accumulatedScore = 0;
-  gameTime = 0;
+  gameTime  = 0;
   laneIndex = 1;
   obstacles = [];
   endScreen = false;
@@ -362,7 +477,9 @@ function resetGame() {
   obstacleTimer = 0;
 }
 
+//--Função usada para pausar quando o utilizador carrega no ESC
 function keyPressed() {
+
   if (keyCode === ESCAPE && !startScreen && !levelSelectScreen && !endScreen && !winScreen) {
     gamePaused = !gamePaused;
     showPopup = !showPopup;
@@ -374,8 +491,10 @@ function keyPressed() {
   }
 }
 
+//--Update dos Top Scores da Sessão de Jogo da altura
 function updateTopScores(newScore) {
   topScores.push(newScore);
   topScores.sort((a, b) => b - a);
   topScores = topScores.slice(0, 3);
 }
+//---------------------------------------Feito por António Rebelo-------------------------------------
